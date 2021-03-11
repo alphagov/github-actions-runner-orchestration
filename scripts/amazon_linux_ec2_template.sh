a04ec9df481e16a6ae738606d82d8765597c2fd0
OllieJC <oliver.chalk@digital.cabinet-office.gov.uk>
2021-03-11 15:14:07 +0000

    tfenv fix

--- scripts/amazon_linux_ec2_template.sh
+++ scripts/amazon_linux_ec2_template.sh
@@ -69,13 +69,13 @@ echo 'github ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/sudoers # pragma: allow
 
 echo "Installing common tools"
 
-sudo amazon-linux-extras enable python3.8
-sudo yum -y install python3.8
+amazon-linux-extras enable python3.8
+yum -y install python3.8
 
-git clone https://github.com/tfutils/tfenv.git ~/.tfenv
-sudo rm /usr/local/bin/tfenv || echo "No tfenv installed"
-sudo rm /usr/local/bin/terraform || echo "No terraform installed"
-sudo ln -s ~/.tfenv/bin/* /usr/local/bin > /dev/null
+git clone https://github.com/tfutils/tfenv.git "/opt/tfenv"
+rm /usr/local/bin/tfenv || echo "No tfenv installed"
+rm /usr/local/bin/terraform || echo "No terraform installed"
+ln -s "/opt/tfenv/bin/*" /usr/local/bin > /dev/null
 
 POETRY_SHA="cc195f1dd086d1c4d12a3acc8d6766981ba431ac" # pragma: allowlist secret
 runuser -l github -c "curl -sSL 'https://raw.githubusercontent.com/python-poetry/poetry/$POETRY_SHA/get-poetry.py' | python -"
